e892e03d56cb2bfb50ba9c64fd3589f65a86d2f0
marcosmpob <marcosmpob@gmail.com>
2020-05-14 07:58:56 -0300

    listagem done

--- src/screens/DoneTasks.js
+++ src/screens/DoneTasks.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 import { Image, StyleSheet, View } from 'react-native';
+import { TaskListView } from '../components/Components';
+import { readTasksFromFirebaseAsync } from '../services/FirebaseApi';
 //const imgDone = require('../assets/done.png');
 
 export default class DoneTasks extends Component {
+
+    state = {
+        tasks: []
+    }
+
     render() {
         return (
-            <View style={styles.conteiner} />
+            <View style={styles.container} >
+                <TaskListView tasks={this.state.tasks} />
+            </View>
         );
+    }
+
+    componentDidMount() {
+        readTasksFromFirebaseAsync(this._fetchTasks.bind(this));
+    }
+    
+    _fetchTasks(tasks) {
+        const tasksToDo = tasks.filter(t => t.isDone);
+        this.setState({ tasks: tasksToDo });
     }
 }
 
